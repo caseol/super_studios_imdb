62708d28d82b6b55787a2f7ed709f95e484dda55
Oliveira <carlos.a.a.oliveira@accenture.com>
2023-03-28 23:55:41 -0300

    Colocando sleep para cada 100 requisições dormir 20 seg

--- super_studios_enrich_multi_threading.py
+++ super_studios_enrich_multi_threading.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 import warnings
 import numpy as np
 import concurrent.futures
@@ -21,20 +22,25 @@ imdb_ids = pd.read_csv("source/ids_imdb.csv")
 imdb_main_parts = np.array_split(imdb_ids, 6)
 # cada código vai usar 1 parte principal que será dividida em 8 sub-partes, para que cada sub-parte
 # possa ser processada por uma thread específica
-imdb_parts = np.array_split(imdb_main_parts[0], 8)
+imdb_parts = np.array_split(imdb_main_parts[1], 8)
 
 # worker que será passada ao ThreadPoolExecutor
 def process_imdb(imdb_ids):
     # instancia objeto para API IMDB
     imdb = IMDb()
-
+    cont = 0
     # lê cada linha do CSV e busca na API do IMDB
     for index, row in imdb_ids.iterrows():
-        if index > 0:
-            print("Lendo linha do CSV - index: " + str(index) + " - row[0]: " + str(row[0]))
+        if index > 0 and cont <= 100:
+            print("Contador: " + str(cont) + " - Lendo linha do CSV - index: " + str(index) + " - row[0]: " + str(row[0]))
             result = imdb.getAllFeatures(row[0], seconds=False)
             print(result)
             queue.put(result)
+            cont += 1
+        else:
+            cont = 0
+            print("Colocando pra dormir")
+            time.sleep(25)
 
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
