53904389375d9a54528ad4fa830e7945287cda6f
Oliveira <carlos.a.a.oliveira@accenture.com>
2023-06-21 00:22:48 -0300

    Defendendo erro para campos maiores que 250 2 + melhorando performance do content_imdb_to_relational_db.py jogando as tabelas auxiliares para dictionary em memoria 2

--- content_imdb_to_relational_db.py
+++ content_imdb_to_relational_db.py
@@ -18,7 +18,7 @@ db_v2 = mysql.connect(
     password="abcd1234",
     database="super_studios_2"
 )
-cursor_v2 = db_v2.cursor(dictionary=True, buffered=True)
+cursor_v2 = db_v2.cursor(buffered=True)
 
 
 def split_and_clean(string):
@@ -43,15 +43,16 @@ def fill_auxiliar_dictionary(table):
     return dict_to_return
 
 
-def create_or_update_model_and_relation(_list, _content_id, _table):
+def create_or_update_model_and_relation(_list, _content_id, _table, _auxiliary_dictionary):
     for item in _list:
         # cursor_v2.execute("select id, name from " + _table + " where name = %s", (item,))
         # if cursor_v2.rowcount > 0:
         # busca item no auxiliar_dictionary para saber se já existe
         #    result = cursor_v2.fetchone()
-        if item in auxiliary_dictionary[_table].keys():
+        item_id = None
+        if item in _auxiliary_dictionary[_table].keys():
             # item já existe pega ID para criar relacionamento
-            item_id = int(auxiliary_dictionary[_table][item])
+            item_id = int(_auxiliary_dictionary[_table][item])
         else:
             # item não existe na tabela cria um novo
             if len(item) <= 150:
@@ -59,10 +60,11 @@ def create_or_update_model_and_relation(_list, _content_id, _table):
                 item_id = cursor_v2.lastrowid
                 db_v2.commit()
                 # inclui no auxiliar_dictionary
-                auxiliary_dictionary[_table][item] = item_id
+                _auxiliary_dictionary[_table][item] = item_id
 
-                # inclui o content_id e item na tabela de relacionamento propria
-        cursor_v2.execute("insert into content_" + _table + " (content_id, " + _table + "_id) values (%s, %s)", [content_id, item_id])
+        if item_id is not None:
+            # inclui o content_id e item na tabela de relacionamento propria
+            cursor_v2.execute("insert into content_" + _table + " (content_id, " + _table + "_id) values (%s, %s)", [_content_id, item_id])
 
 
 print("[" + "{:%Y-%m-%d %H:%M:%S}".format(datetime.now()) + "][auxiliary_dictionary] Inicia o carregamento das tabelas auxiliares")
@@ -76,7 +78,7 @@ auxiliary_dictionary = {'director': fill_auxiliar_dictionary('director'), 'genre
 print("[" + "{:%Y-%m-%d %H:%M:%S}".format(datetime.now()) + "][auxiliary_dictionary] Tabelas auxiliares CARREGADAS!")
 
 # prepara para buscar todos os registros na tabela origem db_v1
-sql_select_v1 = "select * from super_studios.content_imdb where id > 191885"
+sql_select_v1 = "select * from super_studios.content_imdb where id > 198731" #191885 198730
 cursor_v1.execute(sql_select_v1)
 
 # print("Total de registros em super_studios.content_imdb: " + str(cursor_v1.rowcount))
@@ -85,6 +87,7 @@ print("QUERY EXECUTADA!")
 
 # percorre todos os registros do banco origem db_v1 e separa os registros no banco destino db_v2
 resultset_v1 = cursor_v1.fetchall()
+print("[INICIO][" + "{:%Y-%m-%d %H:%M:%S}".format(datetime.now()) + "][REGISTROS CARREGADOS] Total: ")
 for row in resultset_v1:
 
     print("[INICIO][" + "{:%Y-%m-%d %H:%M:%S}".format(datetime.now()) + "][PROCESSANDO] ID: " + str(row['id']) + " - IMDB_ID: " + row['imdb_id'] + " - TITLE: " + row['title'])
@@ -93,47 +96,43 @@ for row in resultset_v1:
     content_id = row['id']
     # pega infos do director e faz o split
     list_directors = split_and_clean(row['director'])
-    create_or_update_model_and_relation(list_directors, content_id, 'director')
+    create_or_update_model_and_relation(list_directors, content_id, 'director', auxiliary_dictionary)
 
     # pega infos do genre (gênero) e faz o split
     list_genres = split_and_clean(row['genre'])
-    create_or_update_model_and_relation(list_genres, content_id, 'genre')
+    create_or_update_model_and_relation(list_genres, content_id, 'genre', auxiliary_dictionary)
 
     # pega infos do creator e faz o split
     list_creator = split_and_clean(row['creator'])
-    create_or_update_model_and_relation(list_creator, content_id, 'creator')
+    create_or_update_model_and_relation(list_creator, content_id, 'creator', auxiliary_dictionary)
 
     # pega infos de actor e faz o split
     list_actor = split_and_clean(row['main_actors'])
-    create_or_update_model_and_relation(list_actor, content_id, 'actor')
+    create_or_update_model_and_relation(list_actor, content_id, 'actor', auxiliary_dictionary)
 
     # pega infos de country e faz o split
     list_country = split_and_clean(row['countries'])
-    create_or_update_model_and_relation(list_country, content_id, 'country')
+    create_or_update_model_and_relation(list_country, content_id, 'country', auxiliary_dictionary)
 
     # pega infos de language e faz o split
     list_language = split_and_clean(row['languages'])
-    create_or_update_model_and_relation(list_language, content_id, 'language')
+    create_or_update_model_and_relation(list_language, content_id, 'language', auxiliary_dictionary)
 
     # pega infos de company e faz o split
     list_company = split_and_clean(row['companies'])
-    create_or_update_model_and_relation(list_company, content_id, 'company')
+    create_or_update_model_and_relation(list_company, content_id, 'company', auxiliary_dictionary)
 
     # pega infos de keyword e faz o split
     list_keyword = split_and_clean(row['keywords'])
-    create_or_update_model_and_relation(list_keyword, content_id, 'keyword')
+    create_or_update_model_and_relation(list_keyword, content_id, 'keyword', auxiliary_dictionary)
 
     # pega infos de film_location e faz o split
     list_film_location = split_and_clean(row['filming_location'])
-    create_or_update_model_and_relation(list_film_location, content_id, 'film_location')
+    create_or_update_model_and_relation(list_film_location, content_id, 'film_location', auxiliary_dictionary)
 
     # pega infos de distributor e faz o split
     list_distributor = split_and_clean(row['distributors'])
-    create_or_update_model_and_relation(list_distributor, content_id, 'distributor')
+    create_or_update_model_and_relation(list_distributor, content_id, 'distributor', auxiliary_dictionary)
 
     db_v2.commit()
     print("[FIM][" + "{:%Y-%m-%d %H:%M:%S}".format(datetime.now()) + "][PROCESSADO] ID: " + str(row['id']) + " - IMDB_ID: " + row['imdb_id'] + " - TITLE: " + row['title'])
-
-
-
-
